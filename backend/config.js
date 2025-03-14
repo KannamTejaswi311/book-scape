a22aaa5ac4895263e098d5ded9ab8c2f52b9f995
KannamTejaswi311 <kannamtejaswi1341@gmail.com>
2025-03-14 20:42:10 +0530

    Updated configuration and package files

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+  'mongodb+srv://tina:tina@book-project.tin1r.mongodb.net/?retryWrites=true&w=majority&appName=Book-project';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
